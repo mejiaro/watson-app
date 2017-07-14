384498743351cd2f895443816e3d65003f3cf9ae
Ricardo Mejía <mejiaro@gmail.com>
2017-07-14 16:22:48 -0600

    verify signature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ShopifyApp::AuthenticatedController
+  include ShopifyApp::AppProxyVerification
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     #173202373
