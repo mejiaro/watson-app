a05c3e5e6d26ff971acde6466ee00fcf9874e59a
Ricardo Mejía <mejiaro@gmail.com>
2017-07-14 17:33:57 -0600

    pass parameters into call

--- app/controllers/kiwi_controller.rb
+++ app/controllers/kiwi_controller.rb
@@ -10,14 +10,18 @@ class KiwiController < ApplicationController
   def suggestions
   	twitter_handle = params[:twitter_handle]
   	#this needs some refactoring bae
-  	client = Twitter::REST::Client.new do |config|
-	  config.consumer_key    = "KzNBGq42kktMYUgKvdL4mLjIG"
+  	@client = Twitter::REST::Client.new do |config|
+	  config.consumer_key    = "KzNBGq42kktMYUgKvdL4mLjIG",
 	  config.consumer_secret = "8q0LffeM9OtCkQu3dP59Qm9SdOIlaMldVHkkRGthq76PY0JqDd"
 	end
 
 	p twitter_handle
 
-	@twit = client.user_timeline(twitter_handle, options = {:count => 200})
+	#un arreglo con 200 tweets
+
+	# text = ""
+
+	# @twit.each do 
 
 
   end
