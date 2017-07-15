453224bf6c9941952d3b30ef49925c1688297bb9
Ricardo Mejía <mejiaro@gmail.com>
2017-07-14 18:25:06 -0600

    render text?

--- app/controllers/kiwi_controller.rb
+++ app/controllers/kiwi_controller.rb
@@ -10,16 +10,28 @@ class KiwiController < ApplicationController
   def suggestions
   	username = params[:twitter_handle]
   	#this needs some refactoring bae
+  	Twitter::REST::Client.new do |config|
+	  config.consumer_key = "KzNBGq42kktMYUgKvdL4mLjIG"
+	  config.consumer_secret = ENV['PASSWORD']
+	end
   	client = Twitter::REST::Client.new do |config|
-	  config.consumer_key    = "KzNBGq42kktMYUgKvdL4mLjIG"
+	  config.consumer_key    = 
 	  config.consumer_secret = "8q0LffeM9OtCkQu3dP59Qm9SdOIlaMldVHkkRGthq76PY0JqDd"
 	end
 
 	p username
 
+	#un arreglo con 200 tweets
 	@tweets = client.user_timeline(username, {:count =>  200})
 
-	#un arreglo con 200 tweets
+
+	@d = ""
+	@tweets.each do |t|
+		@d = @d +' '+ t.full_text
+	end
+	
+
+	
 
 	# text = ""
 
